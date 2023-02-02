ed3224ea2b9da7e0aef7152c8bf46c1b586118a3
khadjaa <islomkhadja2001@icloud.com>
2023-02-02 15:17:47 +0300

    described type MessageType and MessagePropsType and displayed on file Message.tsx. The same changes are made in file FriendMessage.tsx

--- src/s2-homeworks/hw01/friend-message/FriendMessage.tsx
+++ src/s2-homeworks/hw01/friend-message/FriendMessage.tsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import s from './FriendMessage.module.css'
+import {MessageType} from "../HW1";
+
+type FriendMessagePropsType = {
+    message:MessageType
+}
 
 // создать тип вместо any и отобразить приходящие данные
-const FriendMessage = (props: any) => {
+const FriendMessage = (props: FriendMessagePropsType) => {
     return (
         <div
             id={'hw1-friend-message-' + props.message.id}
@@ -12,7 +17,7 @@ const FriendMessage = (props: any) => {
                 <img
                     id={'hw1-friend-avatar-' + props.message.id}
                     // создаёт студент
-
+                    src={props.message.user.avatar}
                     //
                 />
                 <div className={s.friendText}>
@@ -20,7 +25,7 @@ const FriendMessage = (props: any) => {
                         id={'hw1-friend-name-' + props.message.id}
                         className={s.friendName}
                     >
-                        {/*создаёт студент*/}
+                        {props.message.user.name}
 
                         {/**/}
                     </div>
@@ -28,7 +33,7 @@ const FriendMessage = (props: any) => {
                         id={'hw1-friend-text-' + props.message.id}
                         className={s.friendMessageText}
                     >
-                        {/*создаёт студент*/}
+                        {props.message.message.text}
 
                         {/**/}
                     </pre>
@@ -38,7 +43,7 @@ const FriendMessage = (props: any) => {
                 id={'hw1-friend-time-' + props.message.id}
                 className={s.friendTime}
             >
-                {/*создаёт студент*/}
+                {props.message.message.time}
 
                 {/**/}
             </div>
