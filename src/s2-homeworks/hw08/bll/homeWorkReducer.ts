ddea4bc6ebaa217b4ea29fbd6c264dd91b90d40c
khadjaa <islomkhadja2001@icloud.com>
2023-03-22 17:48:00 +0300

    added logic in reducers

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -4,15 +4,18 @@ type ActionType =
     | { type: 'sort'; payload: 'up' | 'down' }
     | { type: 'check'; payload: number }
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
+export const homeWorkReducer = (state: UserType[], action: ActionType): UserType[] => { // need to fix any
     switch (action.type) {
         case 'sort': { // by name
-
-            return state // need to fix
+            if(action.payload === 'up'){
+                return state.map(el => el).sort((a, b)=> b.age - a.age) // need to fix
+            } else if (action.payload === 'down'){
+                return state.map(el => el).sort((a, b)=> a.age - b.age) // need to fix
+            }
+            return state
         }
         case 'check': {
-
-            return state // need to fix
+            return state.filter(el => el.age > 18) // need to fix
         }
         default:
             return state
