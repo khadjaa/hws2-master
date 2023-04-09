bcfc6c8ca52d938798a52d91c772addc0aae094d
khadjaa <islomkhadja2001@icloud.com>
2023-04-09 17:37:38 +0300

    fixed function clearInterval

--- src/s2-homeworks/hw09/Clock.tsx
+++ src/s2-homeworks/hw09/Clock.tsx
@@ -5,7 +5,6 @@ import s from './Clock.module.css'
 
 function Clock() {
     const [timerId, setTimerId] = useState<number | undefined>(undefined)
-    // console.log(!!timerId)
     // for autotests // не менять // можно подсунуть в локалСторэдж нужную дату, чтоб увидеть как она отображается
     const [date, setDate] = useState<Date>(new Date(restoreState('hw9-date', Date.now())))
     const [show, setShow] = useState<boolean>(false)
@@ -13,15 +12,14 @@ function Clock() {
     const start = () => {
         // пишут студенты // запустить часы (должно отображаться реальное время, а не +1)
         // сохранить ид таймера (https://learn.javascript.ru/settimeout-setinterval#setinterval)
-        setTimerId(1)
         let timer = setInterval(() => setDate(new Date(restoreState('hw9-date', Date.now()))), 1000);
-
-        setTimeout(() => { clearInterval(timer); alert('stop'); }, 5000);
+        setTimerId(+timer)
     }
 
     const stop = () => {
         // пишут студенты // поставить часы на паузу, обнулить ид таймера (timerId <- undefined)
-        setTimerId(0)
+        clearInterval(timerId)
+        setTimerId(undefined)
     }
 
     const onMouseEnter = () => { // пишут студенты // показать дату если наведена мышка
@@ -31,8 +29,9 @@ function Clock() {
         setShow(false)
     }
 
-    // const stringTime = new Date().getHours().toString() +':'+  new Date().getMinutes().toString() +':'+  new Date().getSeconds().toString() || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
-    const stringTime = new Intl.DateTimeFormat('ru', {hour:'numeric', minute:'numeric', second:'numeric'}).format() || <br/>
+
+    const stringTime = date.getHours() +':'+  date.getMinutes() +':'+  date.getSeconds() || <br/> // часы24:минуты:секунды (01:02:03)/(23:02:03)/(24:00:00)/(00:00:01) // пишут студенты
+    const stringTime1 = new Intl.DateTimeFormat('ru', {hour:'numeric', minute:'numeric', second:'numeric'}).format() || <br/>
     const stringDate = new Intl.DateTimeFormat('en', {day:'numeric', month: 'numeric', year: 'numeric'}).format() || <br/> // день.месяц.год (01.02.2022) // пишут студенты, варианты 01.02.0123/01.02.-123/01.02.12345 не рассматриваем
 
     // день недели на английском, месяц на английском (https://learn.javascript.ru/intl#intl-datetimeformat)
