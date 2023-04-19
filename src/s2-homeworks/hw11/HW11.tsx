380c253c779daabc984e32c8d46e07be4e98d0aa
khadjaa <islomkhadja2001@icloud.com>
2023-04-19 16:33:26 +0300

    added logic of change function

--- src/s2-homeworks/hw11/HW11.tsx
+++ src/s2-homeworks/hw11/HW11.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, {SetStateAction, useState} from 'react'
 import s from './HW11.module.css'
 import s2 from '../../s1-main/App.module.css'
 import { restoreState } from '../hw06/localStorage/localStorage'
@@ -15,9 +15,24 @@ function HW11() {
     const [value1, setValue1] = useState(restoreState<number>('hw11-value1', 0))
     const [value2, setValue2] = useState(restoreState<number>('hw11-value2', 100))
 
-    const change = (event: any, value: any) => {
+    const change = (event: Event, value: number | Array<number>) => {
         // пишет студент // если пришёл массив - сохранить значения в оба useState, иначе в первый
+        // console.log(value)
+        if (Array.isArray(value)) {
+            setValue1(value[0])
+            setValue2(value[1])
+        } else {
+            setValue1(value)
+        }
+        // setValue(newValue as number[]);
+        // if(typeof value === 'number'){
+        //     setValue1(value)
+        // } else {
+        //     setValue1(value[0])
+        //     setValue2(value[1])
+        // }
     }
+
 
     return (
         <div id={'hw11'}>
@@ -30,7 +45,9 @@ function HW11() {
                         <SuperRange
                             id={'hw11-single-slider'}
                             // сделать так чтоб value1 изменялось // пишет студент
-
+                            // onChange={(e) => change(e, value1)}
+                            onChange={change}
+                            value={value1}
                         />
                     </div>
                     <div className={s.wrapper}>
@@ -38,7 +55,8 @@ function HW11() {
                         <SuperRange
                             id={'hw11-double-slider'}
                             // сделать так чтоб value1/2 изменялось // пишет студент
-
+                            onChange={change}
+                            value={[value1, value2]}
                         />
                         <span id={'hw11-value-2'} className={s.number}>{value2}</span>
                     </div>
